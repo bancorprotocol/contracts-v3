fad50ea91ae6bdc71d551a65f3ec5b191a3db986
Leonid Beder <leonid.beder@gmail.com>
2021-06-18 22:46:50 +0100

    Export Time utils explicitly

--- packages/v3/test/helpers/Time.ts
+++ packages/v3/test/helpers/Time.ts
@@ -1,36 +1,37 @@
 import { ethers } from 'hardhat';
 import { BigNumber, BigNumberish } from 'ethers';
 
-const advanceBlock = async () => {
+export const advanceBlock = async () => {
     return await ethers.provider.send('evm_mine', []);
 };
 
-const latest = async () => {
+export const latest = async () => {
     const block = await ethers.provider.getBlock('latest');
     return BigNumber.from(block.timestamp);
 };
 
-export default {
-    advanceBlock,
-    latest,
-    duration: {
-        seconds: function (val: BigNumberish) {
-            return BigNumber.from(val);
-        },
-        minutes: function (val: BigNumberish) {
-            return BigNumber.from(val).mul(this.seconds('60'));
-        },
-        hours: function (val: BigNumberish) {
-            return BigNumber.from(val).mul(this.minutes('60'));
-        },
-        days: function (val: BigNumberish) {
-            return BigNumber.from(val).mul(this.hours('24'));
-        },
-        weeks: function (val: BigNumberish) {
-            return BigNumber.from(val).mul(this.days('7'));
-        },
-        years: function (val: BigNumberish) {
-            return BigNumber.from(val).mul(this.days('365'));
-        }
-    }
+const seconds = (val: BigNumberish) => {
+    return BigNumber.from(val);
 };
+
+const minutes = (val: BigNumberish) => {
+    return BigNumber.from(val).mul(seconds(60));
+};
+
+const hours = (val: BigNumberish) => {
+    return BigNumber.from(val).mul(minutes(60));
+};
+
+const days = (val: BigNumberish) => {
+    return BigNumber.from(val).mul(hours(24));
+};
+
+const weeks = (val: BigNumberish) => {
+    return BigNumber.from(val).mul(days(7));
+};
+
+const years = (val: BigNumberish) => {
+    return BigNumber.from(val).mul(days(365));
+};
+
+export const duration = { seconds, minutes, hours, days, weeks, years };
