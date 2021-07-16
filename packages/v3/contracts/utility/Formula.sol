cd97c621933308bcfbef403e293a943b9bb487e1
Barak Manos <barakman@yahoo.com>
2021-07-16 15:31:36 +0300

    Improve documentation.

--- packages/v3/contracts/utility/Formula.sol
+++ packages/v3/contracts/utility/Formula.sol
@@ -7,7 +7,7 @@ import "./MathEx.sol";
 import "./Utils.sol";
 
 /**
- * @dev this library provides a set of ???
+ * @dev this library provides a set of functions supporting BNTKN withdrawal
  */
 library Formula {
     using SafeMath for uint256;
@@ -16,6 +16,11 @@ library Formula {
     /**
      * @dev returns `bden(b + c) / {b^3 + b^2(3c - 2e) + b[e^2(n + 1) + c(3c - 4e)] + c(c - e)^2}`
      * by computing `den(b + c) / {b^2 + 3bc + 3c^2 + e^2(n + 1) + c(c^2 + e^2 - 2ce) / b - 2be - 4ce}`
+     *
+     * b = TKN pool balance
+     * d = BNTKN total supply
+     * e = TKN staked amount
+     * n = withdrawal fee in ppm units
      */
     function hMax(uint256 b, uint256 c, uint256 d, uint256 e, uint256 n) internal pure returns (uint256) {
         uint256 z = b.mul(b);
