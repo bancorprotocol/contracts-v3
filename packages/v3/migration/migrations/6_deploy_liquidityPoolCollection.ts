935a3928c85ef77cc217dd6d519ab4fe9c2f5aa7
Maxime Aubanel <maximeauba@gmail.com>
2021-07-29 01:39:35 +0200

    fix typos filename

---
+++ packages/v3/migration/migrations/6_deploy_liquidityPoolCollection.ts
@@ -0,0 +1,26 @@
+import { NextState as InitialState } from './5_deploy_pendingWithdrawals';
+import { deployedContract, Migration } from 'migration/engine/types';
+
+export type NextState = InitialState & {
+    LiquidityPoolCollection: deployedContract;
+};
+
+const migration: Migration = {
+    up: async (signer, contracts, initialState: InitialState, { deploy, execute, createProxy }): Promise<NextState> => {
+        const liquidityPoolCollection = await deploy(contracts.LiquidityPoolCollection, initialState.NetworkSettings);
+        return {
+            ...initialState,
+
+            LiquidityPoolCollection: liquidityPoolCollection.address
+        };
+    },
+
+    healthCheck: async (signer, contracts, state: NextState, { deploy, execute }) => {
+        const liquidityPoolCollection = await contracts.LiquidityPoolCollection.attach(state.LiquidityPoolCollection);
+
+        if ((await liquidityPoolCollection.owner()) !== (await signer.getAddress())) return false;
+
+        return true;
+    }
+};
+export default migration;
