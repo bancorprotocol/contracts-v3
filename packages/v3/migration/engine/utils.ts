b45cef9b129db8cf2d2cc701d577d78e4b138d4a
Maxime Aubanel <maximeauba@gmail.com>
2021-09-09 01:54:31 +0200

    add loader

--- packages/v3/migration/engine/utils.ts
+++ packages/v3/migration/engine/utils.ts
@@ -1,21 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import fs from 'fs-extra';
-import path from 'path';
 
 export const importCsjOrEsModule = (filePath: string) => {
     const imported = require(filePath);
     return imported.default || imported;
-};
-
-export const initEngineAndStartTask = (pathToAction: string) => {
-    return (taskArgs: any, hre: any) => {
-        const actualPath = path.isAbsolute(pathToAction)
-            ? pathToAction
-            : path.join(hre.config.paths.root, pathToAction);
-        const action = importCsjOrEsModule(actualPath);
-        const start = importCsjOrEsModule(path.join(hre.config.paths.root, 'migration/engine/index.ts'));
-        return start(taskArgs, hre, action);
-    };
 };
 
 export const isMigrationFolderValid = (path: string) => {
--- packages/v3/migration/index.ts
+++ packages/v3/migration/index.ts
@@ -1,5 +1,5 @@
+import { loader } from './engine/loader';
 import { defaultArgs } from './engine/types';
-import { initEngineAndStartTask } from './engine/utils';
 import { task, types } from 'hardhat/config';
 import path from 'path';
 
@@ -10,13 +10,13 @@ task('migrate', 'Migrate the network')
     .addFlag('ledger', 'Signing from a ledger')
     .addParam('ledgerPath', 'Ledger path', "m/44'/60'/0'/0", types.string)
     .addParam('gasPrice', 'GasPrice in gwei', 0, types.int)
-    .addParam('confirmationToWait', 'Number of confirmation to wait', 1, types.int)
+    .addParam('minBlockConfirmations', 'Number of confirmation to wait', 1, types.int)
     .addFlag('reset', 'Reset the migration data')
-    .setAction(initEngineAndStartTask(path.join(PATH_TO_TASKS_FOLDER, 'migrate.ts')));
+    .setAction(loader(path.join(PATH_TO_TASKS_FOLDER, 'migrate.ts')));
 
 export type createMigrationParamTask = {
     migrationName: string;
 };
 task('create-migration', 'Create a migration file')
     .addPositionalParam('migrationName', 'Name of the migration name')
-    .setAction(initEngineAndStartTask(path.join(PATH_TO_TASKS_FOLDER, 'createMigration.ts')));
+    .setAction(loader(path.join(PATH_TO_TASKS_FOLDER, 'createMigration.ts')));
