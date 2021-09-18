e5389eda213093e068b2e0530871f92aa5377679
Leonid Beder <leonid.beder@gmail.com>
2021-09-18 21:45:07 +0100

    Style

--- packages/v3/migration/engine/Constants.ts
+++ packages/v3/migration/engine/Constants.ts
@@ -2,8 +2,8 @@ import { MigrationData, SystemDeployments, SystemState } from './Types';
 
 export const MIGRATION_DIR = 'migration/migrations';
 export const MIGRATION_DATA_DIR = 'migration/data';
-export const MIGRATION_DEPLOYMENTS_DIR = 'deployments';
 export const MIGRATION_TASKS_DIR = 'migration/tasks';
+export const MIGRATION_DEPLOYMENTS_DIR = 'deployments';
 
 export const MIGRATION_HISTORY_FILE_NAME = 'history.json';
 export const MIGRATION_STATE_FILE_NAME = 'state.json';
