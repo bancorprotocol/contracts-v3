4534c36c17f243584d3a9a22cbdcf3bd9a158048
Leonid Beder <leonid.beder@gmail.com>
2021-09-18 22:09:58 +0100

    Style

--- packages/v3/migration/index.ts
+++ packages/v3/migration/index.ts
@@ -1,8 +1,8 @@
-import { task, types } from 'hardhat/config';
-import path from 'path';
 import { MIGRATION_TASKS_DIR } from './engine/Constants';
 import { basicTaskLoader, migrationLoader } from './engine/Loaders';
 import { defaultArgs } from './engine/Types';
+import { task, types } from 'hardhat/config';
+import path from 'path';
 
 export type migrateParamTask = defaultArgs;
 
@@ -12,7 +12,7 @@ task('migrate', 'Migrate the network')
     .addParam('gasPrice', 'GasPrice in gwei', 0, types.int)
     .addParam('minBlockConfirmations', 'Number of confirmation to wait', 1, types.int)
     .addFlag('reset', 'Reset the migration data')
-    .setAction(migrationLoader(path.join(MIGRATION_TASKS_DIR, 'migrate.ts')));
+    .setAction(migrationLoader(path.join(MIGRATION_TASKS_DIR, 'Migrate.ts')));
 
 export type createMigrationParamTask = {
     wordList: string[];
