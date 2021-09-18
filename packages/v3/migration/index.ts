2feb57864ab489f46327501d54c375fccf6c1a46
Leonid Beder <leonid.beder@gmail.com>
2021-09-18 21:44:08 +0100

    Refactor

--- packages/v3/migration/index.ts
+++ packages/v3/migration/index.ts
@@ -1,9 +1,8 @@
-import { migrationLoader, basicTaskLoader } from './engine/Loaders';
-import { defaultArgs } from './engine/Types';
 import { task, types } from 'hardhat/config';
 import path from 'path';
-
-export const PATH_TO_TASKS_DIR = 'migration/tasks';
+import { MIGRATION_TASKS_DIR } from './engine/Constants';
+import { basicTaskLoader, migrationLoader } from './engine/Loaders';
+import { defaultArgs } from './engine/Types';
 
 export type migrateParamTask = defaultArgs;
 task('migrate', 'Migrate the network')
@@ -12,11 +11,11 @@ task('migrate', 'Migrate the network')
     .addParam('gasPrice', 'GasPrice in gwei', 0, types.int)
     .addParam('minBlockConfirmations', 'Number of confirmation to wait', 1, types.int)
     .addFlag('reset', 'Reset the migration data')
-    .setAction(migrationLoader(path.join(PATH_TO_TASKS_DIR, 'migrate.ts')));
+    .setAction(migrationLoader(path.join(MIGRATION_TASKS_DIR, 'migrate.ts')));
 
 export type createMigrationParamTask = {
     wordList: string[];
 };
 task('create-migration', 'Create a migration file')
     .addVariadicPositionalParam('wordList', 'Name of the migration')
-    .setAction(basicTaskLoader(path.join(PATH_TO_TASKS_DIR, 'createMigration.ts')));
+    .setAction(basicTaskLoader(path.join(MIGRATION_TASKS_DIR, 'createMigration.ts')));
