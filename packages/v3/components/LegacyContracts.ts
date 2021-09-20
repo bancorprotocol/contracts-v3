d39e333e8d48bb3897a9e3d9ad3c06791d3ac365
Maxime Aubanel <maximeauba@gmail.com>
2021-09-20 20:31:08 +0200

    add legacy BNT and vBNT tokens

--- packages/v3/components/LegacyContracts.ts
+++ packages/v3/components/LegacyContracts.ts
@@ -1,6 +1,10 @@
 /* eslint-disable camelcase */
 import { deployOrAttach } from './ContractBuilder';
-import { TokenGovernance__factory } from '@bancor/token-governance';
+import {
+    TokenGovernance__factory,
+    SmartToken__factory as BNTToken__factory,
+    DSToken__factory as vBNTToken__factory
+} from '@bancor/token-governance';
 
 /* eslint-enable camelcase */
 import { Signer } from '@ethersproject/abstract-signer';
@@ -8,7 +12,9 @@ import { Signer } from '@ethersproject/abstract-signer';
 const getContracts = (signer?: Signer) => ({
     connect: (signer: Signer) => getContracts(signer),
 
-    TokenGovernance: deployOrAttach('TokenGovernance', TokenGovernance__factory, signer)
+    TokenGovernance: deployOrAttach('TokenGovernance', TokenGovernance__factory, signer),
+    BNTToken: deployOrAttach('BNTToken', BNTToken__factory, signer),
+    vBNTToken: deployOrAttach('vBNTToken', vBNTToken__factory, signer)
 
     /* eslint-enable camelcase */
 });
