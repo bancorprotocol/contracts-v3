730cfb94a96d43819849e62238b2ae447cb585ae
Barak Manos <barakman@yahoo.com>
2021-09-21 08:14:13 +0200

    Simplify the Hmax Formula tests.

--- packages/v3/test/pools/PoolCollectionFormulas/HmaxFormula.ts
+++ packages/v3/test/pools/PoolCollectionFormulas/HmaxFormula.ts
@@ -23,35 +23,25 @@ describe('HmaxFormula', () => {
             expected: boolean;
         }
 
-        const tests = (numOfTestsPerFile: number = Number.MAX_SAFE_INTEGER) => {
-            const test = (fileName: string) => {
-                const table: Row[] = JSON.parse(
-                    fs.readFileSync(path.join(__dirname, '../../data', `${fileName}.json`), { encoding: 'utf8' })
-                ).slice(0, numOfTestsPerFile);
+        const tests = (numOfTests: number = Number.MAX_SAFE_INTEGER) => {
+            const table: Row[] = JSON.parse(
+                fs.readFileSync(path.join(__dirname, '../../data/HmaxFormulaCoverage.json'), { encoding: 'utf8' })
+            ).slice(0, numOfTests);
 
-                for (const {b, c, e, m, n, x, expected} of table) {
-                    if (BigNumber.from(b).add(BigNumber.from(c)).gte(BigNumber.from(e))) {
-                        it(`${fileName} surplus(${[b, c, e, m, n, x]})`, async () => {
-                            const actual = await formula.surplus(b, c, e, m, n, x);
-                            expect(actual).to.equal(expected);
-                        });
-                    }
-                    else {
-                        it(`${fileName} deficit(${[b, c, e, m, n, x]})`, async () => {
-                            const actual = await formula.deficit(b, c, e, m, n, x);
-                            expect(actual).to.equal(expected);
-                        });
-                    }
+            for (const {b, c, e, m, n, x, expected} of table) {
+                if (BigNumber.from(b).add(BigNumber.from(c)).gte(BigNumber.from(e))) {
+                    it(`surplus(${[b, c, e, m, n, x]})`, async () => {
+                        const actual = await formula.surplus(b, c, e, m, n, x);
+                        expect(actual).to.equal(expected);
+                    });
                 }
-            };
-
-            test('HmaxFormulaCoverage1');
-            test('HmaxFormulaCoverage2');
-            test('HmaxFormulaCoverage3');
-            test('HmaxFormulaCoverage4');
-            test('HmaxFormulaCoverage5');
-            test('HmaxFormulaCoverage6');
-            test('HmaxFormulaCoverage7');
+                else {
+                    it(`deficit(${[b, c, e, m, n, x]})`, async () => {
+                        const actual = await formula.deficit(b, c, e, m, n, x);
+                        expect(actual).to.equal(expected);
+                    });
+                }
+            }
         };
 
         describe('quick tests', () => {
