829a094446949ddf495dd9eb5c83e4404cbaa87b
Leonid Beder <leonid.beder@gmail.com>
2021-10-02 17:51:10 +0100

    Update hh solidity version

--- packages/v3/hardhat.config.ts
+++ packages/v3/hardhat.config.ts
@@ -33,7 +33,7 @@ const config: HardhatUserConfig = {
     solidity: {
         compilers: [
             {
-                version: '0.7.6',
+                version: '0.8.9',
                 settings: {
                     optimizer: {
                         enabled: true,
