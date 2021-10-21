1da6711eb8b08b8630d95dcac93c6096e82ab7d7
Maxime Aubanel <maximeauba@gmail.com>
2021-10-21 14:58:22 +0200

    update with base branch + typos

--- packages/v3/test/vault/StakingRewardsVault.ts
+++ packages/v3/test/vault/StakingRewardsVault.ts
@@ -1,7 +1,7 @@
 import Contracts from '../../components/Contracts';
 import { NetworkToken } from '../../components/LegacyContracts';
 import { withdrawFundsTest } from '../../test/helpers/Vault';
-import { StakingRewardsVault, TestERC20Token } from '../../typechain';
+import { StakingRewardsVault } from '../../typechain';
 import { expectRole, roles } from '../helpers/AccessControl';
 import { BNT, ETH, TKN } from '../helpers/Constants';
 import { createSystem } from '../helpers/Factory';
@@ -9,48 +9,45 @@ import { prepareEach } from '../helpers/Fixture';
 import { shouldHaveGap } from '../helpers/Proxy';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from 'chai';
-import { BigNumber } from 'ethers';
 import { ethers } from 'hardhat';
 
 const { Upgradeable: UpgradeableRoles, StakingRewardsVault: StakingRewardsVaultRoles } = roles;
 
-let deployer: SignerWithAddress;
-
-let reserveToken: TestERC20Token;
-
 describe('StakingRewardsVault', () => {
+    let deployer: SignerWithAddress;
+
     shouldHaveGap('StakingRewardsVault');
 
     before(async () => {
         [deployer] = await ethers.getSigners();
     });
 
-    prepareEach(async () => {
-        reserveToken = await Contracts.TestERC20Token.deploy(TKN, TKN, BigNumber.from(1_000_000));
-    });
-
     describe('construction', () => {
         it('should revert when attempting to reinitialize', async () => {
-            const { vault } = await createSystem();
+            const { stakingRewardsVault } = await createSystem();
 
-            await expect(vault.initialize()).to.be.revertedWith('Initializable: contract is already initialized');
+            await expect(stakingRewardsVault.initialize()).to.be.revertedWith(
+                'Initializable: contract is already initialized'
+            );
         });
 
         it('should be payable', async () => {
-            const { vault } = await createSystem();
+            const { stakingRewardsVault } = await createSystem();
 
-            expect(await vault.isPayable()).to.be.true;
+            expect(await stakingRewardsVault.isPayable()).to.be.true;
         });
 
         it('should be properly initialized', async () => {
-            const vault = await Contracts.StakingRewardsVault.deploy();
-            await vault.initialize();
+            const stakingRewardsVault = await Contracts.StakingRewardsVault.deploy();
+            await stakingRewardsVault.initialize();
 
-            expect(await vault.version()).to.equal(1);
+            expect(await stakingRewardsVault.version()).to.equal(1);
 
-            await expectRole(vault, UpgradeableRoles.ROLE_ADMIN, UpgradeableRoles.ROLE_ADMIN, [deployer.address]);
+            await expectRole(stakingRewardsVault, UpgradeableRoles.ROLE_ADMIN, UpgradeableRoles.ROLE_ADMIN, [
+                deployer.address
+            ]);
             await expectRole(
-                vault,
+                stakingRewardsVault,
                 StakingRewardsVaultRoles.ROLE_ASSET_MANAGER,
                 StakingRewardsVaultRoles.ROLE_ASSET_MANAGER,
                 [deployer.address]
