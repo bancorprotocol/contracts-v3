87db95ec28a913151d0012bae581d46b12eb44d5
Maxime Aubanel <maximeauba@gmail.com>
2021-10-26 17:13:38 +0200

    updaate should be properly initialized test

--- packages/v3/test/vaults/ExternalProtectionVault.ts
+++ packages/v3/test/vaults/ExternalProtectionVault.ts
@@ -3,7 +3,7 @@ import { NetworkToken } from '../../components/LegacyContracts';
 import { ExternalProtectionVault } from '../../typechain';
 import { expectRole, roles } from '../helpers/AccessControl';
 import { BNT, ETH, TKN } from '../helpers/Constants';
-import { createSystem } from '../helpers/Factory';
+import { createProxy, createSystem } from '../helpers/Factory';
 import { prepareEach } from '../helpers/Fixture';
 import { shouldHaveGap } from '../helpers/Proxy';
 import { transfer, createTokenBySymbol, TokenWithAddress } from '../helpers/Utils';
@@ -16,28 +16,26 @@ const { Upgradeable: UpgradeableRoles, ExternalProtectionVault: ExternalProtecti
 describe('ExternalProtectionVault', () => {
     shouldHaveGap('ExternalProtectionVault');
 
-    before(async () => {});
-
     describe('construction', () => {
-        it('should revert when attempting to reinitialize', async () => {
-            const { externalProtectionVault } = await createSystem();
+        let externalProtectionVault: ExternalProtectionVault;
 
+        prepareEach(async () => {
+            ({ externalProtectionVault } = await createSystem());
+        });
+
+        it('should revert when attempting to reinitialize', async () => {
             await expect(externalProtectionVault.initialize()).to.be.revertedWith(
                 'Initializable: contract is already initialized'
             );
         });
 
         it('should be payable', async () => {
-            const { externalProtectionVault } = await createSystem();
-
             expect(await externalProtectionVault.isPayable()).to.be.true;
         });
 
         it('should be properly initialized', async () => {
             const [deployer] = await ethers.getSigners();
-
-            const externalProtectionVault = await Contracts.ExternalProtectionVault.deploy();
-            await externalProtectionVault.initialize();
+            const externalProtectionVault = await createProxy(Contracts.ExternalProtectionVault);
 
             expect(await externalProtectionVault.version()).to.equal(1);
 
