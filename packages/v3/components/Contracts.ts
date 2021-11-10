8ad574f8756a657894c2bc7b55b8dd84635fc7ec
Leonid Beder <leonid.beder@gmail.com>
2021-11-10 21:51:36 +0000

    Style

--- packages/v3/components/Contracts.ts
+++ packages/v3/components/Contracts.ts
@@ -4,6 +4,7 @@ import {
     BancorVault__factory,
     ERC20__factory,
     ExternalProtectionVault__factory,
+    ExternalRewardsVault__factory,
     NetworkFeeVault__factory,
     NetworkSettings__factory,
     NetworkTokenPool__factory,
@@ -13,7 +14,6 @@ import {
     PoolToken__factory,
     PoolTokenFactory__factory,
     ProxyAdmin__factory,
-    ExternalRewardsVault__factory,
     TestBancorNetwork__factory,
     TestERC20Burnable__factory,
     TestERC20Token__factory,
@@ -28,9 +28,9 @@ import {
     TestReserveToken__factory,
     TestSafeERC20Ex__factory,
     TestUpgradeable__factory,
+    TestVault__factory,
     TokenHolder__factory,
-    TransparentUpgradeableProxy__factory,
-    TestVault__factory
+    TransparentUpgradeableProxy__factory
 } from '../typechain';
 import { deployOrAttach } from './ContractBuilder';
 
@@ -44,6 +44,7 @@ const getContracts = (signer?: Signer) => ({
     BancorVault: deployOrAttach('BancorVault', BancorVault__factory, signer),
     ERC20: deployOrAttach('ERC20', ERC20__factory, signer),
     ExternalProtectionVault: deployOrAttach('ExternalProtectionVault', ExternalProtectionVault__factory, signer),
+    ExternalRewardsVault: deployOrAttach('ExternalRewardsVault', ExternalRewardsVault__factory, signer),
     NetworkFeeVault: deployOrAttach('NetworkFeeVault', NetworkFeeVault__factory, signer),
     NetworkSettings: deployOrAttach('NetworkSettings', NetworkSettings__factory, signer),
     NetworkTokenPool: deployOrAttach('NetworkTokenPool', NetworkTokenPool__factory, signer),
@@ -53,11 +54,14 @@ const getContracts = (signer?: Signer) => ({
     PoolToken: deployOrAttach('PoolToken', PoolToken__factory, signer),
     PoolTokenFactory: deployOrAttach('PoolTokenFactory', PoolTokenFactory__factory, signer),
     ProxyAdmin: deployOrAttach('ProxyAdmin', ProxyAdmin__factory, signer),
-    ExternalRewardsVault: deployOrAttach('ExternalRewardsVault', ExternalRewardsVault__factory, signer),
     TestBancorNetwork: deployOrAttach('TestBancorNetwork', TestBancorNetwork__factory, signer),
     TestERC20Burnable: deployOrAttach('TestERC20Burnable', TestERC20Burnable__factory, signer),
     TestERC20Token: deployOrAttach('TestERC20Token', TestERC20Token__factory, signer),
     TestFlashLoanRecipient: deployOrAttach('TestFlashLoanRecipient', TestFlashLoanRecipient__factory, signer),
+    TestMathEx: deployOrAttach('TestMathEx', TestMathEx__factory, signer),
+    TestNetworkTokenPool: deployOrAttach('TestNetworkTokenPool', TestNetworkTokenPool__factory, signer),
+    TestOwned: deployOrAttach('TestOwned', TestOwned__factory, signer),
+    TestPendingWithdrawals: deployOrAttach('TestPendingWithdrawals', TestPendingWithdrawals__factory, signer),
     TestPoolAverageRate: deployOrAttach('TestPoolAverageRate', TestPoolAverageRate__factory, signer),
     TestPoolCollection: deployOrAttach('TestPoolCollection', TestPoolCollection__factory, signer),
     TestPoolCollectionUpgrader: deployOrAttach(
@@ -65,10 +69,6 @@ const getContracts = (signer?: Signer) => ({
         TestPoolCollectionUpgrader__factory,
         signer
     ),
-    TestNetworkTokenPool: deployOrAttach('TestNetworkTokenPool', TestNetworkTokenPool__factory, signer),
-    TestMathEx: deployOrAttach('TestMathEx', TestMathEx__factory, signer),
-    TestOwned: deployOrAttach('TestOwned', TestOwned__factory, signer),
-    TestPendingWithdrawals: deployOrAttach('TestPendingWithdrawals', TestPendingWithdrawals__factory, signer),
     TestReserveToken: deployOrAttach('TestReserveToken', TestReserveToken__factory, signer),
     TestSafeERC20Ex: deployOrAttach('TestSafeERC20Ex', TestSafeERC20Ex__factory, signer),
     TestUpgradeable: deployOrAttach('TestUpgradeable', TestUpgradeable__factory, signer),
