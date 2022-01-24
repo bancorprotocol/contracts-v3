a4c8d26e18dbfd27ab6d06f24fa0ef5217d8c4e1
Leonid Beder <leonid.beder@gmail.com>
2022-01-24 16:55:49 +0000

    Add AutoCompoundingStakingRewards roles

--- packages/v3/deploy/1642682509-auto-compounding-rewards.ts
+++ packages/v3/deploy/1642682509-auto-compounding-rewards.ts
@@ -1,5 +1,6 @@
 import { ContractName, DeploymentTag } from '../utils/Constants';
-import { deployProxy, DeployedContracts } from '../utils/Deploy';
+import { deployProxy, execute, DeployedContracts } from '../utils/Deploy';
+import { Roles } from '../utils/Roles';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 
@@ -11,10 +12,24 @@ const func: DeployFunction = async ({ getNamedAccounts }: HardhatRuntimeEnvironm
     const networkToken = await DeployedContracts.NetworkToken.deployed();
     const masterPool = await DeployedContracts.MasterPool.deployed();
 
-    await deployProxy({
+    const autoCompoundingRewardsAddress = await deployProxy({
         name: ContractName.AutoCompoundingStakingRewards,
         from: deployer,
         args: [network.address, networkSettings.address, networkToken.address, masterPool.address]
+    });
+
+    await execute({
+        name: ContractName.MasterPool,
+        methodName: 'grantRole',
+        args: [Roles.MasterPool.ROLE_MASTER_POOL_TOKEN_MANAGER, autoCompoundingRewardsAddress],
+        from: deployer
+    });
+
+    await execute({
+        name: ContractName.ExternalRewardsVault,
+        methodName: 'grantRole',
+        args: [Roles.Vault.ROLE_ASSET_MANAGER, autoCompoundingRewardsAddress],
+        from: deployer
     });
 
     return true;
@@ -26,7 +41,8 @@ func.dependencies = [
     ContractName.ProxyAdmin,
     ContractName.BancorNetwork,
     ContractName.NetworkSettings,
-    ContractName.MasterPool
+    ContractName.MasterPool,
+    ContractName.ExternalRewardsVault
 ];
 func.tags = [DeploymentTag.V3, ContractName.AutoCompoundingStakingRewards];
 
--- packages/v3/test/deployment/1642682509-auto-compounding-rewards.ts
+++ packages/v3/test/deployment/1642682509-auto-compounding-rewards.ts
@@ -1,4 +1,9 @@
-import { AutoCompoundingStakingRewards, ProxyAdmin } from '../../components/Contracts';
+import {
+    AutoCompoundingStakingRewards,
+    ExternalRewardsVault,
+    MasterPool,
+    ProxyAdmin
+} from '../../components/Contracts';
 import { ContractName } from '../../utils/Constants';
 import { DeployedContracts, runTestDeployment } from '../../utils/Deploy';
 import { expectRole, Roles } from '../helpers/AccessControl';
@@ -8,6 +13,8 @@ import { getNamedAccounts } from 'hardhat';
 describe('1642682509-auto-compounding-rewards', () => {
     let proxyAdmin: ProxyAdmin;
     let deployer: string;
+    let masterPool: MasterPool;
+    let externalRewardsVault: ExternalRewardsVault;
     let autoCompoundingStakingRewards: AutoCompoundingStakingRewards;
 
     before(async () => {
@@ -18,6 +25,8 @@ describe('1642682509-auto-compounding-rewards', () => {
         await runTestDeployment(ContractName.AutoCompoundingStakingRewards);
 
         proxyAdmin = await DeployedContracts.ProxyAdmin.deployed();
+        masterPool = await DeployedContracts.MasterPool.deployed();
+        externalRewardsVault = await DeployedContracts.ExternalRewardsVault.deployed();
         autoCompoundingStakingRewards = await DeployedContracts.AutoCompoundingStakingRewards.deployed();
     });
 
@@ -28,6 +37,17 @@ describe('1642682509-auto-compounding-rewards', () => {
 
         await expectRole(autoCompoundingStakingRewards, Roles.Upgradeable.ROLE_ADMIN, Roles.Upgradeable.ROLE_ADMIN, [
             deployer
+        ]);
+
+        await expectRole(autoCompoundingStakingRewards, Roles.Upgradeable.ROLE_ADMIN, Roles.Upgradeable.ROLE_ADMIN, [
+            deployer
+        ]);
+
+        await expectRole(masterPool, Roles.MasterPool.ROLE_MASTER_POOL_TOKEN_MANAGER, Roles.Upgradeable.ROLE_ADMIN, [
+            autoCompoundingStakingRewards.address
+        ]);
+        await expectRole(externalRewardsVault, Roles.Vault.ROLE_ASSET_MANAGER, Roles.Upgradeable.ROLE_ADMIN, [
+            autoCompoundingStakingRewards.address
         ]);
     });
 });
