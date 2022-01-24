aa379339efbff13a09eea9cd86f6dcd0a3b04de5
Leonid Beder <leonid.beder@gmail.com>
2022-01-24 12:50:13 +0000

    Remove unchecked from ERC20Burnable

--- packages/v3/test/token/ERC20Burnable.ts
+++ packages/v3/test/token/ERC20Burnable.ts
@@ -102,7 +102,7 @@ describe('ERC20Burnable', () => {
 
             await burnable.connect(owner).approve(burner.address, amount);
             await expect(burnable.connect(owner).burnFrom(owner.address, amount)).to.be.revertedWith(
-                'InsufficientAllowance'
+                'reverted with panic code 0x11 (Arithmetic operation underflowed or overflowed outside of an unchecked block)'
             );
         });
 
@@ -111,7 +111,7 @@ describe('ERC20Burnable', () => {
 
             await burnable.connect(owner).approve(burner.address, allowance);
             await expect(burnable.connect(owner).burnFrom(owner.address, allowance + 1)).to.be.revertedWith(
-                'InsufficientAllowance'
+                'reverted with panic code 0x11 (Arithmetic operation underflowed or overflowed outside of an unchecked block)'
             );
         });
     });
