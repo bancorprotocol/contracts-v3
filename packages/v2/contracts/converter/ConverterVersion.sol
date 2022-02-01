6a5be2b14afac036efcd4db848985a0256dc97a8
Leonid Beder <leonid.beder@gmail.com>
2022-02-01 21:04:50 +0000

    Remove V2 code and flatten V3

--- packages/v2/contracts/converter/ConverterVersion.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.6.12;
-
-contract ConverterVersion {
-    // note that the version is defined as is for backward compatibility with older converters
-
-    // solhint-disable-next-line const-name-snakecase
-    uint16 public constant version = 47;
-}
--- packages/v2/contracts/helpers/TestContractRegistryClient.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.6.12;
-
-import "../utility/ContractRegistryClient.sol";
-
-contract TestContractRegistryClient is ContractRegistryClient {
-    constructor(IContractRegistry registry) public ContractRegistryClient(registry) {}
-}
