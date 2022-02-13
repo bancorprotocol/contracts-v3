9279b7a058406c5b5f768e9f699196df34e0f491
Leonid Beder <leonid.beder@gmail.com>
2022-02-13 10:54:41 +0000

    Style

--- test/network/NetworkSettings.ts
+++ test/network/NetworkSettings.ts
@@ -6,7 +6,6 @@ import { createSystem, createTestToken } from '../helpers/Factory';
 import { shouldHaveGap } from '../helpers/Proxy';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from 'chai';
-import { BigNumber } from 'ethers';
 import { ethers } from 'hardhat';
 
 describe('NetworkSettings', () => {
