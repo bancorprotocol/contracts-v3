c34d1eb7530a935a39e9f194eaa0ed8bb395a0b5
Leonid Beder <leonid.beder@gmail.com>
2022-02-15 23:54:19 +0000

    Add network fee management

--- utils/Roles.ts
+++ utils/Roles.ts
@@ -9,7 +9,8 @@ export const Roles = {
 
     BancorNetwork: {
         ROLE_MIGRATION_MANAGER: id('ROLE_MIGRATION_MANAGER'),
-        ROLE_EMERGENCY_STOPPER: id('ROLE_EMERGENCY_STOPPER')
+        ROLE_EMERGENCY_STOPPER: id('ROLE_EMERGENCY_STOPPER'),
+        ROLE_NETWORK_FEE_MANAGER: id('ROLE_NETWORK_FEE_MANAGER')
     },
 
     MasterVault: {
