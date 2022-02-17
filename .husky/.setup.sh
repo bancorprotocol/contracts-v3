096c8a2611dc0103012a3a13d855ac5e70008fb0
John Doe <john@doe.com>
2022-02-17 09:29:33 -0600

    removed git hooks

--- .husky/.setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-. "$(dirname "$0")/_/husky.sh"
-
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/homebrew/bin:$PATH"
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
