67a83f48bc1af1562e80109760e22f9a42c6fcbe
John Doe <john@doe.com>
2022-02-21 15:54:05 -0600

    more styling

--- deployments/data.ts
+++ deployments/data.ts
@@ -28,7 +28,7 @@ const UniswapNamedAccounts = {
     uniswapV2Factory: { ...mainnet('0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f') }
 };
 
-const SushiswapNamedAccounts = {
+const SushiSwapNamedAccounts = {
     sushiSwapRouter: { ...mainnet('0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F') },
     sushiSwapFactory: { ...mainnet('0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac') }
 };
@@ -40,7 +40,7 @@ export const NamedAccounts = {
     ...LegacyNamedAccounts,
     ...TestNamedAccounts,
     ...UniswapNamedAccounts,
-    ...SushiswapNamedAccounts
+    ...SushiSwapNamedAccounts
 };
 
 export const ExternalContracts = {
