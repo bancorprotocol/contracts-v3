6ae18bb6eed806d0682221a15180a43e141ff504
Leonid Beder <leonid.beder@gmail.com>
2022-02-21 16:51:08 +0000

    Remove only

--- test/network/BancorNetwork.ts
+++ test/network/BancorNetwork.ts
@@ -2521,7 +2521,7 @@ describe('BancorNetwork', () => {
                                     });
 
                                     if (!isSourceNativeToken) {
-                                        context.only('with an insufficient approval', () => {
+                                        context('with an insufficient approval', () => {
                                             it('should revert when attempting to trade', async () => {
                                                 const missingAmount = 10;
 
