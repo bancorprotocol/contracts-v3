b0091fc6c8581e4b611e01025efdbfecbf9fc904
Leonid Beder <leonid.beder@gmail.com>
2022-03-16 22:02:01 +0000

    Don't override _initialized

--- contracts/helpers/TestLogic.sol
+++ contracts/helpers/TestLogic.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.11;
 import { Upgradeable } from "../utility/Upgradeable.sol";
 
 contract TestLogic is Upgradeable {
-    bool private _initialized;
+    bool private _initializedLogic;
     uint16 private _version;
 
     uint256[MAX_GAP - 1] private __gap;
@@ -20,14 +20,14 @@ contract TestLogic is Upgradeable {
     }
 
     function __TestLogic_init_unchained() internal onlyInitializing {
-        _initialized = true;
+        _initializedLogic = true;
         _version = 1;
     }
 
     // solhint-enable func-name-mixedcase
 
     function initialized() external view returns (bool) {
-        return _initialized;
+        return _initializedLogic;
     }
 
     function version() external view override returns (uint16) {
