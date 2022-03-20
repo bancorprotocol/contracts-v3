eaf988417daa4d61f3f828eb855a5f349a9bdaa1
Leonid Beder <leonid.beder@gmail.com>
2022-03-20 23:10:41 +0000

    Style

--- contracts/staking-rewards/StandardStakingRewards.sol
+++ contracts/staking-rewards/StandardStakingRewards.sol
@@ -526,7 +526,9 @@ contract StandardStakingRewards is IStandardStakingRewards, ReentrancyGuardUpgra
 
             if (i == 0) {
                 rewardsToken = p.rewardsToken;
-            } else if (p.rewardsToken != rewardsToken) {
+            }
+
+            if (p.rewardsToken != rewardsToken) {
                 revert RewardsTokenMismatch();
             }
 
@@ -741,7 +743,9 @@ contract StandardStakingRewards is IStandardStakingRewards, ReentrancyGuardUpgra
             if (i == 0) {
                 rewardData.pool = p.pool;
                 rewardData.rewardsToken = p.rewardsToken;
-            } else if (p.rewardsToken != rewardData.rewardsToken) {
+            }
+
+            if (p.rewardsToken != rewardData.rewardsToken) {
                 revert RewardsTokenMismatch();
             }
 
