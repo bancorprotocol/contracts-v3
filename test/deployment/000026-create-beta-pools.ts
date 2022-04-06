a53c73dbac60415c964bf6f1a4c012d0a8627337
Leonid Beder <leonid.beder@gmail.com>
2022-04-06 18:47:01 +0100

    No need to skip this test

--- test/deployment/000026-create-beta-pools.ts
+++ test/deployment/000026-create-beta-pools.ts
@@ -7,73 +7,69 @@ import { describeDeployment } from '../helpers/Deploy';
 import { expect } from 'chai';
 import { getNamedAccounts } from 'hardhat';
 
-describeDeployment(
-    __filename,
-    () => {
-        let networkSettings: NetworkSettingsV1;
-        let poolCollection: PoolCollection;
+describeDeployment(__filename, () => {
+    let networkSettings: NetworkSettingsV1;
+    let poolCollection: PoolCollection;
 
-        // TODO: make sure to update the limits and the rates before running the script in production
-        const CENTS = 100;
-        const BNT_TOKEN_PRICE_IN_CENTS = 2.7 * CENTS;
+    // TODO: make sure to update the limits and the rates before running the script in production
+    const CENTS = 100;
+    const BNT_TOKEN_PRICE_IN_CENTS = 2.7 * CENTS;
 
-        const TRADING_FEE = toPPM(0.2);
+    const TRADING_FEE = toPPM(0.2);
 
-        enum BetaTokens {
-            ETH = 'ETH',
-            DAI = 'DAI',
-            LINK = 'LINK'
-        }
+    enum BetaTokens {
+        ETH = 'ETH',
+        DAI = 'DAI',
+        LINK = 'LINK'
+    }
 
-        const BETA_TOKEN_PRICES_IN_CENTS = {
-            [BetaTokens.ETH]: 3266 * CENTS,
-            [BetaTokens.DAI]: 1 * CENTS,
-            [BetaTokens.LINK]: 15.67 * CENTS
+    const BETA_TOKEN_PRICES_IN_CENTS = {
+        [BetaTokens.ETH]: 3266 * CENTS,
+        [BetaTokens.DAI]: 1 * CENTS,
+        [BetaTokens.LINK]: 15.67 * CENTS
+    };
+
+    const TKN_DEPOSIT_LIMIT_IN_CENTS = 171_000 * CENTS;
+    const BNT_FUNDING_LIMIT_IN_CENTS = 156_000 * CENTS;
+    const FUNDING_LIMIT = toWei(BNT_FUNDING_LIMIT_IN_CENTS).div(BNT_TOKEN_PRICE_IN_CENTS);
+
+    beforeEach(async () => {
+        networkSettings = await DeployedContracts.NetworkSettingsV1.deployed();
+        poolCollection = await DeployedContracts.PoolCollectionType1V1.deployed();
+    });
+
+    it.only('should create beta pools', async () => {
+        const { dai, link } = await getNamedAccounts();
+
+        const minLiquidityForTrading = await networkSettings.minLiquidityForTrading();
+
+        const BETA_TOKENS = {
+            [BetaTokens.ETH]: NATIVE_TOKEN_ADDRESS,
+            [BetaTokens.DAI]: dai,
+            [BetaTokens.LINK]: link
         };
 
-        const TKN_DEPOSIT_LIMIT_IN_CENTS = 171_000 * CENTS;
-        const BNT_FUNDING_LIMIT_IN_CENTS = 156_000 * CENTS;
-        const FUNDING_LIMIT = toWei(BNT_FUNDING_LIMIT_IN_CENTS).div(BNT_TOKEN_PRICE_IN_CENTS);
+        for (const [tokenSymbol, address] of Object.entries(BETA_TOKENS)) {
+            expect(await networkSettings.isTokenWhitelisted(address)).to.be.true;
+            expect(await networkSettings.poolFundingLimit(address)).to.equal(FUNDING_LIMIT);
 
-        beforeEach(async () => {
-            networkSettings = await DeployedContracts.NetworkSettingsV1.deployed();
-            poolCollection = await DeployedContracts.PoolCollectionType1V1.deployed();
-        });
+            const tokenPriceInCents = BETA_TOKEN_PRICES_IN_CENTS[tokenSymbol as BetaTokens];
+            const depositLimit = toWei(TKN_DEPOSIT_LIMIT_IN_CENTS).div(tokenPriceInCents);
 
-        it('should create beta pools', async () => {
-            const { dai, link } = await getNamedAccounts();
+            const data = await poolCollection.poolData(address);
+            expect(data.depositLimit).to.equal(depositLimit);
+            expect(data.tradingFeePPM).to.equal(TRADING_FEE);
 
-            const minLiquidityForTrading = await networkSettings.minLiquidityForTrading();
+            const bntVirtualPrice = tokenPriceInCents;
+            const tokenVirtualPrice = BNT_TOKEN_PRICE_IN_CENTS;
+            const initialDeposit = minLiquidityForTrading.mul(tokenVirtualPrice).div(bntVirtualPrice).mul(3);
 
-            const BETA_TOKENS = {
-                [BetaTokens.ETH]: NATIVE_TOKEN_ADDRESS,
-                [BetaTokens.DAI]: dai,
-                [BetaTokens.LINK]: link
-            };
+            expect(data.liquidity.stakedBalance).to.equal(initialDeposit);
+            expect(data.liquidity.baseTokenTradingLiquidity).to.equal(
+                data.liquidity.bntTradingLiquidity.mul(tokenVirtualPrice).div(bntVirtualPrice)
+            );
 
-            for (const [tokenSymbol, address] of Object.entries(BETA_TOKENS)) {
-                expect(await networkSettings.isTokenWhitelisted(address)).to.be.true;
-                expect(await networkSettings.poolFundingLimit(address)).to.equal(FUNDING_LIMIT);
-
-                const tokenPriceInCents = BETA_TOKEN_PRICES_IN_CENTS[tokenSymbol as BetaTokens];
-                const depositLimit = toWei(TKN_DEPOSIT_LIMIT_IN_CENTS).div(tokenPriceInCents);
-
-                const data = await poolCollection.poolData(address);
-                expect(data.depositLimit).to.equal(depositLimit);
-                expect(data.tradingFeePPM).to.equal(TRADING_FEE);
-
-                const bntVirtualPrice = tokenPriceInCents;
-                const tokenVirtualPrice = BNT_TOKEN_PRICE_IN_CENTS;
-                const initialDeposit = minLiquidityForTrading.mul(tokenVirtualPrice).div(bntVirtualPrice).mul(3);
-
-                expect(data.liquidity.stakedBalance).to.equal(initialDeposit);
-                expect(data.liquidity.baseTokenTradingLiquidity).to.equal(
-                    data.liquidity.bntTradingLiquidity.mul(tokenVirtualPrice).div(bntVirtualPrice)
-                );
-
-                expect(data.tradingEnabled).to.be.true;
-            }
-        });
-    },
-    () => !isMainnetFork()
-);
+            expect(data.tradingEnabled).to.be.true;
+        }
+    });
+});
