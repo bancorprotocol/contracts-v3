69b5180befa1e36c748ac36e35a9001057a9edb0
Barak Manos <barakman@yahoo.com>
2022-04-25 09:36:27 +0300

    Extend the financial verification tests by adding a test of funding-limit reduction after trading is enabled.

--- test/network/BancorNetwork.ts
+++ test/network/BancorNetwork.ts
@@ -4113,6 +4113,10 @@ describe('BancorNetwork Financial Verification', () => {
             .tradeBySourceAmount(bnt.address, baseToken.address, wei, 1, MAX_UINT256, users[userId].address);
     };
 
+    const setFundingLimit = async (amount: string) => {
+        await networkSettings.setFundingLimit(baseToken.address, decimalToInteger(amount, bntDecimals));
+    };
+
     const enableTrading = async (bntVirtualBalance: number, baseTokenVirtualBalance: number) => {
         await poolCollection.enableTrading(baseToken.address, bntVirtualBalance, baseTokenVirtualBalance);
     };
@@ -4285,10 +4289,13 @@ describe('BancorNetwork Financial Verification', () => {
                     await tradeBNT(userId, amount);
                     break;
 
+                case 'setFundingLimit':
+                    await setFundingLimit(amount);
+                    break;
+
                 case 'enableTrading': {
                     const { bntVirtualBalance, baseTokenVirtualBalance } = amount as any;
                     await enableTrading(bntVirtualBalance, baseTokenVirtualBalance);
-
                     break;
                 }
             }
@@ -4315,6 +4322,7 @@ describe('BancorNetwork Financial Verification', () => {
         test('BancorNetworkSimpleFinancialScenario2');
         test('BancorNetworkSimpleFinancialScenario3');
         test('BancorNetworkSimpleFinancialScenario4');
+        test('BancorNetworkReduceFundingLimit');
     });
 
     describe('@stress test', () => {
