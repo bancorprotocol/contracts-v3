b865772f514b8faddced8bc379bea11be4f6136a
Leonid Beder <leonid.beder@gmail.com>
2022-05-04 00:02:47 +0100

    Update research fork creation

--- deployments/setup-fork.ts
+++ deployments/setup-fork.ts
@@ -128,7 +128,7 @@ const removeDepositLimits = async (tokens: string[]) => {
 
     const { daoMultisig } = await getNamedSigners();
 
-    const poolCollection = await DeployedContracts.PoolCollectionType1V1.deployed();
+    const poolCollection = await DeployedContracts.PoolCollectionType1V2.deployed();
     for (const token of tokens) {
         await poolCollection.connect(daoMultisig).setDepositLimit(token, MAX_UINT256);
     }
