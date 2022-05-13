6b54db2db6d1184c286299789018d7507bc60639
Leonid Beder <>
2022-05-13 12:41:55 +0100

    Use latest types

--- deploy/scripts/000039-upgrade-network-info-v2.ts
+++ deploy/scripts/000039-upgrade-network-info-v2.ts
@@ -5,16 +5,16 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 const func: DeployFunction = async ({ getNamedAccounts }: HardhatRuntimeEnvironment) => {
     const { deployer } = await getNamedAccounts();
 
-    const network = await DeployedContracts.BancorNetworkV1.deployed();
+    const network = await DeployedContracts.BancorNetwork.deployed();
     const bntGovernance = await DeployedContracts.BNTGovernance.deployed();
     const vbntGovernance = await DeployedContracts.VBNTGovernance.deployed();
-    const networkSettings = await DeployedContracts.NetworkSettingsV1.deployed();
+    const networkSettings = await DeployedContracts.NetworkSettings.deployed();
     const masterVault = await DeployedContracts.MasterVault.deployed();
     const externalProtectionVault = await DeployedContracts.ExternalProtectionVault.deployed();
     const externalRewardsVault = await DeployedContracts.ExternalRewardsVault.deployed();
     const bntPool = await DeployedContracts.BNTPool.deployed();
-    const pendingWithdrawals = await DeployedContracts.PendingWithdrawalsV1.deployed();
-    const poolMigrator = await DeployedContracts.PoolMigratorV1.deployed();
+    const pendingWithdrawals = await DeployedContracts.PendingWithdrawals.deployed();
+    const poolMigrator = await DeployedContracts.PoolMigrator.deployed();
 
     await upgradeProxy({
         name: InstanceName.BancorNetworkInfo,
