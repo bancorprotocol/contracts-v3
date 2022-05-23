f526b8fa51bbe1d3250dff354b4cf182ff4648ec
Leonid Beder <>
2022-05-23 13:48:01 +0100

    Remove unneeded types

--- deployments/mainnet/types/index.ts
+++ deployments/mainnet/types/index.ts
@@ -1,9 +1,7 @@
 /* eslint-disable camelcase */
 
 export type { PoolCollection as PoolCollectionType1V2 } from './contracts/pools/PoolCollectionType1V2';
-export type { PoolMigrator as PoolMigratorV1 } from './contracts/pools/PoolMigratorV1';
 
 export { PoolCollection__factory as PoolCollectionType1V2__factory } from './factories/contracts/pools/PoolCollectionType1V2__factory';
-export { PoolMigrator__factory as PoolMigratorV1__factory } from './factories/contracts/pools/PoolMigratorV1__factory';
 
 /* eslint-enable camelcase */
