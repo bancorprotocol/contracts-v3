6f9a6e6c8a0de82523ad267035f3bf22d48cf5e9
Leonid Beder <>
2022-06-06 23:00:00 +0100

    Update script

--- scripts/find-pending-pools.ts
+++ scripts/find-pending-pools.ts
@@ -1,20 +1,13 @@
 import Contracts from '../components/Contracts';
-import { DeployedContracts } from '../utils/Deploy';
+import { DeployedContracts, getNamedSigners } from '../utils/Deploy';
 import Logger from '../utils/Logger';
 import { DEFAULT_DECIMALS, NATIVE_TOKEN_ADDRESS, TokenSymbol } from '../utils/TokenData';
 import '@nomiclabs/hardhat-ethers';
 import '@typechain/hardhat';
-import axios from 'axios';
+import { CoinGeckoClient } from 'coingecko-api-v3';
 import Decimal from 'decimal.js';
 import fs from 'fs';
 import path from 'path';
-
-interface EnvOptions {
-    CRYPTOCOMPARE_API_KEY: string;
-}
-
-const { CRYPTOCOMPARE_API_KEY }: EnvOptions = process.env as any as EnvOptions;
-const CRYPTOCOMPARE_API = 'https://min-api.cryptocompare.com/data/price';
 
 interface TokenOverride {
     address: string;
@@ -26,22 +19,38 @@ const TOKEN_OVERRIDES: TokenOverride[] = JSON.parse(
     fs.readFileSync(path.resolve(__dirname, '../data/token-overrides.json'), 'utf-8')
 );
 
-const LOCAL_TOKEN_SYMBOL_OVERRIDES: Record<string, string> = {
-    '0xd5cd84d6f044abe314ee7e414d37cae8773ef9d3': 'ONE'
-};
-
 const MIN_STAKED_BALANCE_FACTOR = 2;
 
 const main = async () => {
+    const { deployer } = await getNamedSigners();
+    const bnt = await DeployedContracts.BNT.deployed();
     const network = await DeployedContracts.BancorNetwork.deployed();
     const networkInfo = await DeployedContracts.BancorNetworkInfo.deployed();
     const networkSettings = await DeployedContracts.NetworkSettings.deployed();
 
+    const allPools = await network.liquidityPools();
+
+    const client = new CoinGeckoClient({
+        timeout: 10000,
+        autoRetry: true
+    });
+
+    /* eslint-disable camelcase */
+    const tokenPrices = await client.simpleTokenPrice({
+        id: 'ethereum',
+        contract_addresses: [bnt.address, ...allPools].join(','),
+        vs_currencies: 'USD'
+    });
+    /* eslint-enable camelcase */
+
+    const bntPrice = new Decimal(tokenPrices[bnt.address.toLowerCase()].usd);
+
     Logger.log();
     Logger.log('Looking for disabled pools...');
 
-    const allPools = await network.liquidityPools();
     const minLiquidityForTrading = await networkSettings.minLiquidityForTrading();
+
+    const unknownTokens: Record<string, string> = {};
 
     const pools: Record<string, string> = {};
     for (let i = 0; i < allPools.length; i++) {
@@ -53,10 +62,9 @@ const main = async () => {
             symbol = TokenSymbol.ETH;
             decimals = DEFAULT_DECIMALS;
         } else {
-            const localSymbolOverride = LOCAL_TOKEN_SYMBOL_OVERRIDES[pool.toLowerCase()];
             const tokenOverride = TOKEN_OVERRIDES.find((t) => t.address.toLowerCase() === pool.toLowerCase());
-            const token = await Contracts.ERC20.attach(pool);
-            symbol = localSymbolOverride ?? tokenOverride?.symbol ?? (await token.symbol());
+            const token = await Contracts.ERC20.attach(pool, deployer);
+            symbol = tokenOverride?.symbol ?? (await token.symbol());
             decimals = tokenOverride?.decimals ?? (await token.decimals());
         }
 
@@ -76,24 +84,20 @@ const main = async () => {
             continue;
         }
 
-        const res = await axios.get(CRYPTOCOMPARE_API, {
-            params: {
-                fsym: TokenSymbol.BNT,
-                tsyms: symbol
-            },
-            headers: {
-                Apikey: CRYPTOCOMPARE_API_KEY
-            }
-        });
+        const tokenPriceData = tokenPrices[pool.toLowerCase()];
+        if (!tokenPriceData) {
+            unknownTokens[symbol] = pool;
 
-        const rate = res.data[symbol];
-        if (!rate) {
             Logger.error('  Skipping unknown token');
 
             continue;
         }
+        const tokenPrice = new Decimal(tokenPriceData.usd);
+        const rate = bntPrice.div(tokenPrice);
 
-        Logger.log(`  ${symbol} to ${TokenSymbol.BNT} rate: ${rate}`);
+        Logger.log(`  ${TokenSymbol.BNT} price: $${bntPrice.toFixed(4)}`);
+        Logger.log(`  ${symbol} price: $${tokenPrice.toFixed(4)}`);
+        Logger.log(`  ${symbol} to ${TokenSymbol.BNT} rate: ${rate.toFixed(4)}`);
 
         const rateScale = new Decimal(10).pow(DEFAULT_DECIMALS - decimals);
         const estimatedRequiredLiquidity = new Decimal(minLiquidityForTrading.toString())
@@ -102,13 +106,13 @@ const main = async () => {
             .div(rateScale);
         const decimalsScale = new Decimal(10).pow(decimals);
 
-        Logger.log(`  Current staked ${symbol} balance (wei): ${stakedBalance.toFixed()}`);
-        Logger.log(`  Current staked ${symbol} balance: ${stakedBalance.div(decimalsScale).toFixed()}`);
-        Logger.log(`  Estimating minimum required ${symbol} liquidity (wei): ${estimatedRequiredLiquidity.toFixed()}`);
+        Logger.log(`  Current staked ${symbol} balance (wei): ${stakedBalance.toFixed(4)}`);
+        Logger.log(`  Current staked ${symbol} balance: ${stakedBalance.div(decimalsScale).toFixed(4)}`);
+        Logger.log(`  Estimating minimum required ${symbol} liquidity (wei): ${estimatedRequiredLiquidity.toFixed(4)}`);
         Logger.log(
             `  Estimating minimum required ${symbol} liquidity: ${estimatedRequiredLiquidity
                 .div(decimalsScale)
-                .toFixed()}`
+                .toFixed(4)}`
         );
 
         if (stakedBalance.lt(estimatedRequiredLiquidity)) {
@@ -144,6 +148,16 @@ const main = async () => {
     Logger.log('');
     Logger.log('********************************************************************************');
     Logger.log('');
+
+    if (Object.keys(unknownTokens).length !== 0) {
+        Logger.log('Unknown tokens:');
+
+        for (const [symbol, address] of Object.entries(unknownTokens)) {
+            Logger.log(`${symbol} - ${address}`);
+        }
+
+        Logger.log('');
+    }
 };
 
 main()
