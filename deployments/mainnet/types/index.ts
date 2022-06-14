279ba8ddb9838ecadeff8d07003ad838c562b013
Leonid Beder <>
2022-06-14 14:15:33 +0100

    Update types

--- deployments/mainnet/types/index.ts
+++ deployments/mainnet/types/index.ts
@@ -1,7 +1,9 @@
 /* eslint-disable camelcase */
 
 export type { PoolCollection as PoolCollectionType1V5 } from './contracts/pools/PoolCollectionType1V5';
+export type { PoolCollection as PoolCollectionType1V6 } from './contracts/pools/PoolCollectionType1V6';
 
 export { PoolCollection__factory as PoolCollectionType1V5__factory } from './factories/contracts/pools/PoolCollectionType1V5__factory';
+export { PoolCollection__factory as PoolCollectionType1V6__factory } from './factories/contracts/pools/PoolCollectionType1V6__factory';
 
 /* eslint-enable camelcase */
