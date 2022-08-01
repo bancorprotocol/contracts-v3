88b95132643d5c70cb537c91289df145e287deda
Yudi Levi <yudi@bancor.network>
2022-08-01 09:31:56 +0100

    Enabled standard rewards deploy scripts

--- deploy/scripts/000055-upgrade-standard-rewards-v3.ts
+++ deploy/scripts/000055-upgrade-standard-rewards-v3.ts
@@ -28,6 +28,4 @@ const func: DeployFunction = async ({ getNamedAccounts }: HardhatRuntimeEnvironm
     return true;
 };
 
-func.skip = async () => true;
-
 export default setDeploymentMetadata(__filename, func);
--- deploy/tests/000055-upgrade-standard-rewards-v3.ts
+++ deploy/tests/000055-upgrade-standard-rewards-v3.ts
@@ -4,28 +4,23 @@ import { describeDeployment } from '../../test/helpers/Deploy';
 import { DeployedContracts } from '../../utils/Deploy';
 import { expect } from 'chai';
 
-describeDeployment(
-    __filename,
-    () => {
-        let standardRewards: StandardRewards;
-        let externalAutoCompoundingRewardsVault: ExternalRewardsVault;
+describeDeployment(__filename, () => {
+    let standardRewards: StandardRewards;
+    let externalAutoCompoundingRewardsVault: ExternalRewardsVault;
 
-        beforeEach(async () => {
-            standardRewards = await DeployedContracts.StandardRewards.deployed();
-            externalAutoCompoundingRewardsVault =
-                await DeployedContracts.ExternalAutoCompoundingRewardsVault.deployed();
-        });
+    beforeEach(async () => {
+        standardRewards = await DeployedContracts.StandardRewards.deployed();
+        externalAutoCompoundingRewardsVault = await DeployedContracts.ExternalAutoCompoundingRewardsVault.deployed();
+    });
 
-        it('should upgrade the standard rewards contract', async () => {
-            expect(await standardRewards.version()).to.equal(4);
+    it('should upgrade the standard rewards contract', async () => {
+        expect(await standardRewards.version()).to.equal(4);
 
-            const programIds = await standardRewards.programIds();
-            for (const id of programIds) {
-                expect(await standardRewards.isProgramPaused(id)).to.be.false;
-            }
+        const programIds = await standardRewards.programIds();
+        for (const id of programIds) {
+            expect(await standardRewards.isProgramPaused(id)).to.be.false;
+        }
 
-            await expectRoleMembers(externalAutoCompoundingRewardsVault, Roles.Vault.ROLE_ASSET_MANAGER);
-        });
-    },
-    { skip: () => true }
-);
+        await expectRoleMembers(externalAutoCompoundingRewardsVault, Roles.Vault.ROLE_ASSET_MANAGER);
+    });
+});
