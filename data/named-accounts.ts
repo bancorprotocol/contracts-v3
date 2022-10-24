809ab51cc062ce2516777d341455f55c1e357ad2
Adam Shai <adam.shai@mail.huji.ac.il>
2022-10-24 15:16:18 +0300

    Update DAI whale account
    Update DAI whale account, because the previous did not have DAI

--- data/named-accounts.ts
+++ data/named-accounts.ts
@@ -14,7 +14,7 @@ const TestNamedAccounts = {
         ...mainnet('0xDA9dfA130Df4dE4673b89022EE50ff26f6EA73Cf')
     },
     daiWhale: {
-        ...mainnet('0x5D38B4e4783E34e2301A2a36c39a03c45798C4dD')
+        ...mainnet('0x428AB2BA90Eba0a4Be7aF34C9Ac451ab061AC010')
     },
     linkWhale: {
         ...mainnet('0xc6bed363b30DF7F35b601a5547fE56cd31Ec63DA')
