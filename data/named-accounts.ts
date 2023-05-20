820093e66619f7671552184e5eccc62989153579
Yudi Levi <yudi@bancor.network>
2023-05-20 07:23:11 +0100

    updated mainnet addresses

--- data/named-accounts.ts
+++ data/named-accounts.ts
@@ -35,16 +35,16 @@ const TokenNamedAccounts = {
         ...mainnet('0x6B175474E89094C44Da98b954EedeAC495271d0F')
     },
     link: {
-        ...mainnet('0x514910771AF9Ca656af840dff83E8264EcF986CA')
+        ...mainnet('0x0757e27AC1631beEB37eeD3270cc6301dD3D57D4')
     },
     weth: {
         ...mainnet('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
     },
     usdc: {
-        ...mainnet('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48')
+        ...mainnet('0x51eDF02152EBfb338e03E30d65C15fBf06cc9ECC')
     },
     wbtc: {
-        ...mainnet('0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599')
+        ...mainnet('0x6daB3bCbFb336b29d06B9C793AEF7eaA57888922')
     }
 };
 
