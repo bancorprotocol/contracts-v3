4b92c2af4ef032b920a12cdf28219c5225c66404
Ivan Zhelyazkov <ivan.y.zhelyazkov@gmail.com>
2024-10-15 19:42:19 +0300

    add fee exemption whitelist - deployment scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -124,7 +124,7 @@ const config: HardhatUserConfig = {
                 settings: {
                     optimizer: {
                         enabled: true,
-                        runs: 200
+                        runs: 180
                     },
                     metadata: {
                         bytecodeHash: 'none'
